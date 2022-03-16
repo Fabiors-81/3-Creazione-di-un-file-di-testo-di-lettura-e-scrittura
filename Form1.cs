0cc673edd878f6bf0fcd0e2b954cb3e0058d6225
ElEkTrOnIc <75856311+Fabiors-81@users.noreply.github.com>
2022-03-16 19:27:41 +0100

    Creazione di un form da dove vengono richiamati: l'head di html, il body; e la chiusura.

--- Form1.cs
+++ Form1.cs
@@ -10,13 +10,27 @@ namespace FilediTesto
 
         private void button1_Click(object sender, EventArgs e)
         {
-            gestioneFile.scrivi_fileditesto(textBox1.Text,@"c:\test\","filediTesto","csv");
-             
+            //gestioneFile.scrivi_fileditesto(textBox1.Text, @"c:\test\", "filediTesto", "csv");
+            gestioneFile.scrivi_fileditesto(html(), Directory.GetCurrentDirectory(), "FileTesto", "html");
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-              textBox1.Text = gestioneFile.leggi_fileditesto(@"c:\test\","filediTesto","csv");//Directory.GetCurrentDirectory());
+            textBox1.Text = gestioneFile.leggi_fileditesto(@"c:\test\", "filediTesto", "csv");//Directory.GetCurrentDirectory());
+        }
+
+        private string html()
+        {
+            HtmlGenera htmlGenera = new();
+            string result = null;
+            //Scrivo la testata, apertura html
+            result = htmlGenera.testa("Benvenuto");
+            result += htmlGenera.body();
+            result += htmlGenera.close();
+            return result;
+            //Chiusura dell'html
+            
         }
     }
 }
---
+++ HtmlGenera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilediTesto
+{
+    internal class HtmlGenera
+    {
+        public string testa(string titolo)
+        {
+            string result = "";
+            result = "<html>";
+            result += "<head>";
+            result += "<title>"+titolo+"</title>";
+            result += "</head>";
+            return result;
+
+        }
+
+        public string body()
+        {
+            string result = "";
+            //Corpo dell'html
+            result += "<body>";
+
+            //Collegamento a youtube
+            result += "<iframe src='http://www.ecowebhosting.co.uk'></iframe>";
+
+            result += "<iframe src='helloworld.html' frameborder='0'></iframe>";
+            result += "<iframe width='569' height='315'";
+
+            //Collegamento a youtube
+            result += "src='http://www.youtube.com/embed/IjS950fumU8?rel=0' frameborder='0'";
+
+            result += "allowfullscreen></iframe>";
+            result += "<iframe width='560' height='315' src='https://www.youtube.com/embed/jLozON01eRM'";
+            result += "frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' ";
+            result += "allowfullscreen></iframe>";
+            result += "<iframe width='560' height='315' src='https://www.youtube.com/embed/yksM7Ym2yTU?start=61'";
+            result += "frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'";
+            result += "Saveallowfullscreen></iframe>>";
+            result += "</body>";
+            return result;
+        }
+
+        public string close()
+        {
+            string result = "";
+            result += "</html>";
+            return result;
+        }
+    }
+}
+
